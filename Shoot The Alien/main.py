df6dc5fe9f92b4f98a4b2c31681234ebe4e066ca
Arcticfox712 <Pragyasrivastava@gmail.com>
2025-08-11 18:41:18 +0300

    Add files via upload

--- Shoot The Alien/main.py
+++ Shoot The Alien/main.py
@@ -1,21 +1,43 @@
 import pgzrun
+#import pygame
 import random
 WIDTH=500
 HEIGHT=500
+message= ""
+score=0
 TITLE="Shoot The Alien"
+#redball=Actor("redballhw")
 alien=Actor("alien")
 alien.pos=100, 100
+#redball.pos=200, 200
+#redball._surf = pygame.transform.scale (redball._surf, (100, 100))
 
 
 def place_alien():
     alien.x=random.randint(100, 400)
     alien.y=random.randint(100, 400)
+#def place_redball():
+#    redball.x= 20
+#    redball.y= 20
 def draw():
     screen.fill(color=(10, 0, 150))
+    
     alien.draw()
+    screen.draw.text(message, center=(300,10))
+    screen.draw.text ("score"+str(score), center=(50, 20))
+#  redball.draw()
+
+def on_mouse_down(pos):
+    global message, score
+    if alien.collidepoint(pos):
+        message="good shot!"
+        score=score+1
+        place_alien()
+    else:
+        message="you missed"
     
  
- 
+#place_redball() 
  
 place_alien() 
 pgzrun.go()
